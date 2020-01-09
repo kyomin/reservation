b373807aa4ef584eed60ade1a8b472446cb8e9f8
kyomin <kim031504@nts-corp.com>
2020-01-09 12:16:35 +0900

    코드 정리2
    1. TodayServlet 클래스 내에서 <br/> 테그를 반복하는 불필요한 메서드 제거
    
    2. margin 속성으로 위치관계 재정리
    
    3. html 에서 <style> 내의 내용들 CSS 파일로 분리 작업 요함

--- aboutme/src/aboutme/TodayServlet.java
+++ aboutme/src/aboutme/TodayServlet.java
@@ -21,16 +21,6 @@ import com.sun.xml.internal.ws.util.StringUtils;
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    public String stringRepeat(String str, int n) {
-    	String result = "";
-    	
-    	for(int i=0; i<n; i++) {
-    		result += str;
-    	}
-    	
-    	return result;
-    }
-
     /*
   		http://localhost:8080/aboutme/today의 경로에 대해 GET Request를 처리하는 메소드이다.
   		클라이언트에게 GET 요청을 한 시점의 시각을 제공한다.
@@ -46,15 +36,9 @@ public class TodayServlet extends HttpServlet {
 		out.println("<head><title>현재 시간</title></head>");
 		out.println("<body>");
 		out.println("<a href='/aboutme/index.html'>메인화면</a>");
-		
-		// 15번 개행!
-		out.println(stringRepeat("<br/>", 15));
-		
-		out.println("<h1 style='text-align:center;font-size:50pt'>" +
-				"현재시간 : " + currentDateTime + "</h1>");
+		out.println("<h1 style='text-align:center;font-size:50pt;margin-top:300px'>" + "현재시간 : " + currentDateTime + "</h1>");
 		out.println("</body>");
 		out.println("</html>");
-		
 		
 		// 스트림 닫기
 		out.close();
