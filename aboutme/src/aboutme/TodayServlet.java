130ce0c1da73fea9a53fa3eb1ace144f29c4cebc
kyomin <kim031504@nts-corp.com>
2020-01-09 12:07:52 +0900

    코드 정리
    1. 쓰이지 않는 불필요한 코드 제거(디폴트 생성자)
    
    2. img 태그 한 줄로 정리
    
    3. 들여쓰기 정리
    
    4. 클래스 내의 오버라이딩 된 메서드 어노테이션으로 표기
    
    5. 클래스 및 메서드의 용도 주석으로 표기
    
    6. 리다이렉트 부분 함수 정의가 아닌 href로 구현하여 불필요한 스크립트
    파일 제거
    
    7. 도메인 포함하여 링크 타는 부분 URI만 명시하여 진행

--- aboutme/src/aboutme/TodayServlet.java
+++ aboutme/src/aboutme/TodayServlet.java
@@ -1,5 +1,6 @@
 package aboutme;
 
+import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
@@ -11,16 +12,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.sun.xml.internal.ws.util.StringUtils;
 
+/*
+  	http://localhost:8080/aboutme/today의 경로를 타고 들어오는 서비스를 처리하는 자바 서블릿 클래스이다.
+*/
 @WebServlet("/today")
 public class TodayServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    
-    public TodayServlet() {
+    public String stringRepeat(String str, int n) {
+    	String result = "";
+    	
+    	for(int i=0; i<n; i++) {
+    		result += str;
+    	}
+    	
+    	return result;
     }
 
-	
+    /*
+  		http://localhost:8080/aboutme/today의 경로에 대해 GET Request를 처리하는 메소드이다.
+  		클라이언트에게 GET 요청을 한 시점의 시각을 제공한다.
+    */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
@@ -28,23 +43,20 @@ public class TodayServlet extends HttpServlet {
 		String currentDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
 		
 		out.println("<html>");
-		out.println("<head><title>today</title></head>");
+		out.println("<head><title>현재 시간</title></head>");
 		out.println("<body>");
+		out.println("<a href='/aboutme/index.html'>메인화면</a>");
 		
-		out.println("<a href='http://localhost:8080/aboutme/index.html'>메인화면</a>");
-		
-		// 5번 개행!
-		for(int i=0; i<15; i++)
-		{
-			out.println("<br/>");
-		}
+		// 15번 개행!
+		out.println(stringRepeat("<br/>", 15));
 		
 		out.println("<h1 style='text-align:center;font-size:50pt'>" +
 				"현재시간 : " + currentDateTime + "</h1>");
-		
 		out.println("</body>");
 		out.println("</html>");
 		
+		
+		// 스트림 닫기
 		out.close();
 	}
 }
