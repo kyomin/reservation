3214ee4e333c4afbdbb1908afddec47bb7da21e3
kyomin <kim031504@nts-corp.com>
2020-01-09 11:03:14 +0900

    출근 후 첫 작업
    1. 서블릿 파일 작성 및 모든 기능구현 완료
    
    2. 전체적인 코드 정리 요함

--- aboutme/src/aboutme/TodayServlet.java
+++ aboutme/src/aboutme/TodayServlet.java
@@ -2,6 +2,8 @@ package aboutme;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -23,11 +25,22 @@ public class TodayServlet extends HttpServlet {
 		response.setContentType("text/html;charset=utf-8");
 		PrintWriter out = response.getWriter();
 		
+		String currentDateTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"));
+		
 		out.println("<html>");
 		out.println("<head><title>today</title></head>");
 		out.println("<body>");
 		
 		out.println("<a href='http://localhost:8080/aboutme/index.html'>메인화면</a>");
+		
+		// 5번 개행!
+		for(int i=0; i<15; i++)
+		{
+			out.println("<br/>");
+		}
+		
+		out.println("<h1 style='text-align:center;font-size:50pt'>" +
+				"현재시간 : " + currentDateTime + "</h1>");
 		
 		out.println("</body>");
 		out.println("</html>");
