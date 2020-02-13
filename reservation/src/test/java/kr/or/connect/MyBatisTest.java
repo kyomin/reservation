a1c8193a63b7462cfebac7167503c007febc6bd7
kyomin <kim031504@naver.com>
2020-02-13 11:32:43 +0900

    마이바티스 설정 및 테스트 완료. 추후 코드 적용 요함

---
+++ reservation/src/test/java/kr/or/connect/MyBatisTest.java
@@ -0,0 +1,32 @@
+package kr.or.connect;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/applicationContext.xml"})
+public class MyBatisTest {
+	@Inject
+    private SqlSessionFactory sqlSessionFactory;
+
+    @Test
+    public void testFactory(){
+        System.out.println(sqlSessionFactory);
+    }
+
+    @Test
+    public void testSession() throws Exception{
+        try{
+            SqlSession sqlSession = sqlSessionFactory.openSession();
+            System.out.println(sqlSession);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
